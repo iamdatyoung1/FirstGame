c241e4f719bb3f43557815880b8ba2fcd24121e6
iamdatyoung1 <mistaking_stone@hotmail.ca>
2022-05-19 12:36:36 -0400

    First commit
    All default assests

---
+++ Source/FirstUnrealGame/FirstUnrealGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstUnrealGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstUnrealGame, "FirstUnrealGame" );
---
+++ Source/FirstUnrealGame/FirstUnrealGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstUnrealGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTUNREALGAME_API AFirstUnrealGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
